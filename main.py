100fc2943d66f0dcf11682adcf7bbafbb978b1c2
pancho123123 <154936494+pancho123123@users.noreply.github.com>
2025-08-06 10:12:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -103,12 +103,12 @@ class Pokemon(pygame.sprite.Sprite):
                if pygame.time.get_ticks() - self.ultimo_ataque > self.tiempo_entre_ataques:
                   pok.hp -= damage(self,pok)
                   self.ultimo_ataque = pygame.time.get_ticks()
-      else:
-         for poke in all_sprites:
-            if poke.team_int == 1:
-               if pygame.time.get_ticks() - self.ultimo_ataque > self.tiempo_entre_ataques:
-                  poke.hp -= damage(self,poke)
-                  self.ultimo_ataque = pygame.time.get_ticks()
+      # else:
+      #    for poke in all_sprites:
+      #       if poke.team_int == 1:
+      #          if pygame.time.get_ticks() - self.ultimo_ataque > self.tiempo_entre_ataques:
+      #             poke.hp -= damage(self,poke)
+      #             self.ultimo_ataque = pygame.time.get_ticks()
                
             
 
